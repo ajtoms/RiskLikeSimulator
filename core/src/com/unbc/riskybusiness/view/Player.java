d4dc489bba0384b1286dc967134e9fcb986357e6
lee-mapjungle <leefoster@Lees-MacBook-Pro.local>
2015-03-27 03:25:44 -0700

    Forgot to commit Player.java

---
+++ core/src/com/unbc/riskybusiness/view/Player.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unbc.riskybusiness.view;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.unbc.riskybusiness.agents.Agent;
+
+/**
+ *
+ * @author leefoster
+ */
+public class Player {
+    BitmapFont font = null;
+    Texture pieceTexture = null;
+    PlayerUI ui = null;
+    Agent agent = null;
+    
+    String unitCount = "10";
+    float scale = 0.0f;
+    
+    public Player(PlayerColor color, float scale, Agent agent,BitmapFont font) {
+        
+        switch (color) {
+            case BLUE: pieceTexture = new Texture("pieceBlue.png"); break;
+            case RED: pieceTexture = new Texture("pieceRed.png"); break;
+            case YELLOW: pieceTexture = new Texture("pieceYellow.png"); break;
+            case GREEN: pieceTexture = new Texture("pieceGreen.png"); break;
+        }
+        this.agent = agent;
+        this.scale = scale;
+        this.font = font;
+        
+        ui = new PlayerUI(color, font);
+    }
+    
+    /* batch.begin() is called in GameScreen draw() so it doesn't need to be 
+       called here.
+    */
+    public void draw(Batch batch, OrthographicCamera camera) {
+        ui.update(camera);
+        ui.draw(batch);
+    }
+    
+    public void startTurn() {
+        ui.setTexture(ui.enabled);
+    }
+    
+    public void endTurn() {
+        ui.setTexture(ui.disabled);
+    }
+    
+}
