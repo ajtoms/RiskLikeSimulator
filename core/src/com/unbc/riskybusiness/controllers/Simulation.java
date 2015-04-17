16258c8512b8734775969d089ccccd2525d22e0f
lee-mapjungle <leefoster@Lees-MacBook-Pro.local>
2015-04-17 03:15:45 -0700

    Compile Fixes
    Minor fixes so compiling works

--- core/src/com/unbc/riskybusiness/controllers/Simulation.java
+++ core/src/com/unbc/riskybusiness/controllers/Simulation.java
@@ -1,5 +1,6 @@
 package com.unbc.riskybusiness.controllers;
 
+import com.unbc.riskybusiness.agents.AbstractAgent;
 import com.unbc.riskybusiness.agents.Agent;
 import com.unbc.riskybusiness.main.Logger;
 import java.util.HashMap;
@@ -15,44 +16,44 @@ import java.util.HashMap;
  */
 public class Simulation {
 
-    public Simulation(Agent a, Agent b, Agent c, Agent d, int numGames){
+    public Simulation(AbstractAgent a, AbstractAgent b, AbstractAgent c, AbstractAgent d, int numGames){
         
         //Use a HashMap as a scoreboard
-        HashMap<Agent, Integer> results = new HashMap<Agent, Integer>();
+        HashMap<AbstractAgent, Integer> results = new HashMap<AbstractAgent, Integer>();
         results.put(a, 0);
         results.put(b, 0);
         results.put(c, 0);
         results.put(d, 0);
         
-        long allTimes = 0;
-        for(int i = 0; i < numGames; i++){
-            
-            Game g = new Game(a,b,c,d);
-            a.setGame(g);
-            b.setGame(g);
-            c.setGame(g);
-            d.setGame(g);
-            
-            long time = System.nanoTime();
-            Agent winner = g.play();
-            time = System.nanoTime() - time;
-            allTimes += time;
-            
-            results.put(winner, results.get(winner) + 1);
-        }
-        
-        
-        allTimes /= numGames;
-        double avgTimeSeconds = allTimes / 1000000;
-        Logger.startLog(false);
-        Logger.log("Simulation Results");
-        Logger.log(String.format("Average Runtime: %.4fms", avgTimeSeconds));
-        for(Agent ag : results.keySet()){
-            int resultsAg = results.get(ag);
-            double percentAg = (double) resultsAg / (double) numGames;
-            String s = String.format("%s won %.2f%% of games (%d)", ag, percentAg, resultsAg);
-            Logger.log(s);
-        }
+//        long allTimes = 0;
+//        for(int i = 0; i < numGames; i++){
+//            
+////            Game g = new Game(a,b,c,d);
+////            a.setGame(g);
+////            b.setGame(g);
+////            c.setGame(g);
+////            d.setGame(g);
+////            
+//            long time = System.nanoTime();
+//            Agent winner = g.play();
+//            time = System.nanoTime() - time;
+//            allTimes += time;
+//            
+//            results.put(winner, results.get(winner) + 1);
+//        }
+//        
+//        
+//        allTimes /= numGames;
+//        double avgTimeSeconds = allTimes / 1000000;
+//        Logger.startLog(false);
+//        Logger.log("Simulation Results");
+//        Logger.log(String.format("Average Runtime: %.4fms", avgTimeSeconds));
+//        for(Agent ag : results.keySet()){
+//            int resultsAg = results.get(ag);
+//            double percentAg = (double) resultsAg / (double) numGames;
+//            String s = String.format("%s won %.2f%% of games (%d)", ag, percentAg, resultsAg);
+//            Logger.log(s);
+//        }
     }
     
 }
